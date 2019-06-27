557ac5d0b7cab43a5ad0595beb7834d199b5bd89
guispada <gui.spada13@gmail.com>
2019-06-26 21:12:28 -0300

    With Logo

--- app/src/main/java/com/example/dothis/Lista.java
+++ app/src/main/java/com/example/dothis/Lista.java
@@ -80,13 +80,13 @@ public class Lista extends AppCompatActivity {
         }
     }
 
-    @Override
+    /*@Override
     public void onBackPressed() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-    }
+    }*/
 
     public void cadastrar(View view){
 
